0a7f589aa18f8d3a55d2653e21119584202da731
Dezkiel <dezkiel@gmail.com>
2023-01-07 19:37:37 +0100

    Added winner function resetting the game

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let playerScore = 0;
 let computerScore = 0;
+let computerSelection;
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+const containerDiv = document.querySelector('.score-message-container');
+const playerScoreMsg = document.querySelector('.player-score');
+const computerScoreMsg = document.querySelector('.computer-score');
+const drawMsg = document.querySelector('.draw-msg')
 
 function getComputerChoice()
 {
@@ -21,50 +29,101 @@ function getComputerChoice()
 
 function playRound(playerSelection, computerSelection)
 {
-    playerSelection.toLowerCase();
-
     switch(playerSelection)
     {
         case "rock":
             if(computerSelection === "paper")
+            {
                 computerScore++;
+                computerScoreMsg.textContent = `Computer score: ${computerScore}`;
+            }
             else if (computerSelection === "scissors")
+            {
                 playerScore++;
+                playerScoreMsg.textContent = `Player score: ${playerScore}`;
+            }
+            else if (computerSelection === "rock")
+            {
+                drawMsg.textContent = "It's a draw";
+            }
             break;
         case "paper":
             if(computerSelection === "rock")
+            {
                 playerScore++;
+                playerScoreMsg.textContent = `Player score: ${playerScore}`;
+            }
             else if (computerSelection === "scissors")
-                computerScore++ ;
+            {
+                computerScore++;
+                computerScoreMsg.textContent = `Computer score: ${computerScore}`;
+            }
+            else if(computerSelection === "paper")
+            {
+                drawMsg.textContent = "It's a draw";
+            }
             break;
         case "scissors":
             if(computerSelection === "paper")
+            {
                 playerScore++;
+                playerScoreMsg.textContent = `Player score: ${playerScore}`;
+            }
             else if (computerSelection === "rock")
+            {
                 computerScore++;
+                computerScoreMsg.textContent = `Computer score: ${computerScore}`;
+            }
+            else if(computerSelection === "scissors")
+            {
+                drawMsg.textContent = "It's a draw";
+            }
             break;
     }
 }
 
-function game()
+function winner()
 {
-    for(let i = 1; i <= 5; i++)
-    {        
-        let playerSelection = prompt("Rock, Paper of Scissors?");
-        let computerSelection = getComputerChoice();
-
-        playRound(playerSelection, computerSelection);
-
-        console.log(`Round ${i}, Player Score: ${playerScore} and
-        Computer Score ${computerScore}`);
-    }
-
-    if(playerScore < computerScore)
-        console.log(`Computer wins with ${computerScore} points!`);
-    else if(playerScore > computerScore)
-        console.log(`Players wins with ${playerScore} points!`);
-    else
-        console.log("It's a draw!");
+    if(playerScore === 5)
+        drawMsg.textContent = "You win";
+    else if(computerScore === 5)
+        drawMsg.textContent = "You loose";
+        
+    playerScore = 0;
+    computerScore = 0;        
+    playerScoreMsg.textContent = `Player score: ${playerScore}`;
+    computerScoreMsg.textContent = `Computer score: ${computerScore}`;
+    
 }
 
-game();
+rockBtn.addEventListener('click', () =>
+{
+    computerSelection = getComputerChoice();
+    playRound("rock", computerSelection);
+    if(playerScore === 5 || computerScore === 5)
+    {
+        winner();
+    }
+});
+
+paperBtn.addEventListener('click', () =>
+{
+    computerSelection = getComputerChoice();
+    playRound("paper", computerSelection);
+    if(playerScore === 5 || computerScore === 5)
+    {
+        winner();
+    }
+});
+
+scissorsBtn.addEventListener('click', () =>
+{
+    computerSelection = getComputerChoice();
+    playRound("scissors", computerSelection);
+    if(playerScore === 5 || computerScore === 5)
+    {
+        winner();
+    }
+});
+
+
